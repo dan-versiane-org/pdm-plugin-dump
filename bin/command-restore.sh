b877b92739664c99fde04c7b9aca297bba598a63
Daniel Versiane <daniel.versiane@gmail.com>
2024-06-24 11:35:06 -0300

    feat: add param latest on unzip command

--- bin/command-restore.sh
+++ bin/command-restore.sh
@@ -5,10 +5,19 @@ handle::dump::restore() {
   local DUMP_OUTPUT_PATH="${PDM_WORKSPACE_CURRENT_ROOT}/backup"
   # TODO: add pass to env
   local DUMP_ZIP_PASS="PASS123"
+  local FILE_TO_UNZIP="${DUMP_OUTPUT_PATH}/${1}"
+
+  if [ "${1}" == "latest" ]; then
+    latest_file=$(ls -t ${DUMP_OUTPUT_PATH}/*.zip | head -n 1)
+    latest_file_name=$(basename "${latest_file}" .zip)
+    FILE_TO_UNZIP="${DUMP_OUTPUT_PATH}/${latest_file_name}"
+  else
+    FILE_TO_UNZIP="${DUMP_OUTPUT_PATH}/${1}"
+  fi
 
   # TODO: add if exists params
   # TODO: add if exists file dump
-  unzip -P "${DUMP_ZIP_PASS}" -o -d "${PDM_WORKSPACE_CURRENT_ROOT}/projects" "${DUMP_OUTPUT_PATH}/${1}"
+  unzip -P "${DUMP_ZIP_PASS}" -o -d "${PDM_WORKSPACE_CURRENT_ROOT}/projects" "${FILE_TO_UNZIP}"
 }
 
 handle::dump::restore $@
